8860b433e53817849a969d50b6aed2a2fc36c3a8
atakannerturk <46032754+atakannerturk@users.noreply.github.com>
2021-10-14 10:10:19 +0300

    Update post controller

--- kiyafet uyg backend/Controllers/PostController.cs
+++ kiyafet uyg backend/Controllers/PostController.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace kiyafet_uyg_backend.Controllers
 {
-    public class PostController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
     {
+
     }
 }
